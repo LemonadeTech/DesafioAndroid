1df9b1f3d94c0347534f674458a32a42580cd86c
Rodrigo Oliveira <rodrigo@deadbodyoutline.com>
2016-01-16 02:00:20 -0200

    Adding weatherLib as dependency
    My app is based on their demo app (called 'demo' :p)
    
    Signed-off-by: Rodrigo Oliveira <rodrigo@deadbodyoutline.com>

--- app/src/main/java/org/thecoreme/rodrigo/desafioandroid/MainActivity.java
+++ app/src/main/java/org/thecoreme/rodrigo/desafioandroid/MainActivity.java
@@ -1,20 +1,86 @@
 package org.thecoreme.rodrigo.desafioandroid;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.survivingwithandroid.weather.lib.WeatherClient;
+import com.survivingwithandroid.weather.lib.WeatherConfig;
+import com.survivingwithandroid.weather.lib.exception.WeatherLibException;
+import com.survivingwithandroid.weather.lib.model.CurrentWeather;
+import com.survivingwithandroid.weather.lib.provider.openweathermap.OpenweathermapProviderType;
+import com.survivingwithandroid.weather.lib.request.WeatherRequest;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ///
+        try {
+            //        WeatherClient.ClientBuilder builder = new WeatherClient.ClientBuilder();
+            //        WeatherConfig config = new WeatherConfig();
+            // using OpenWeatherMap and a synchronous http client
+            //        WeatherClient client = builder.attach(getActivity())
+            //                .provider(new OpenweathermapProviderType())
+            //                .httpClient(com.survivingwithandroid.weather.lib.StandardHttpClient.class)
+            //                .config(config)
+            //
+            WeatherClient client = (new WeatherClient.ClientBuilder()).attach(this)
+                    .httpClient(WeatherDefaultClient.class)
+                    .provider(new OpenweathermapProviderType())
+                    .config(new WeatherConfig())
+                    .build();
+
+            // use it on the settings activity
+//            config.unitSystem = WeatherConfig.UNIT_SYSTEM.M;
+//            config.lang = "en";
+//            config.maxResult = 5;
+//            config.numDays = 6;
+//            client.updateWeatherConfig(config);
+
+            client.getCurrentCondition(new WeatherRequest("2988507"), new WeatherClient.WeatherEventListener() {
+                @Override
+                public void onWeatherRetrieved(CurrentWeather currentWeather) {
+                    float currentTemp = currentWeather.weather.temperature.getTemp();
+                    Log.d("WL", "City [" + currentWeather.weather.location.getCity() + "] Current temp [" + currentTemp + "]");
+                }
+
+                @Override
+                public void onWeatherError(WeatherLibException e) {
+                    Log.d("WL", "Weather Error - parsing data");
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onConnectionError(Throwable throwable) {
+                    Log.d("WL", "Connection error");
+                    throwable.printStackTrace();
+                }
+            });
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        ///
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +92,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -48,5 +117,45 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.thecoreme.rodrigo.desafioandroid/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.thecoreme.rodrigo.desafioandroid/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
